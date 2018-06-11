9509fd55edf993c082e01b5cbf7e0920efc678e9
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-06-11 02:36:18 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -13,10 +13,10 @@ class Book
     @author
   end
   def page_count=(num)
-    @page_count = page_count
+    @page_count = num
   end
   def page_count
-    @page_count = num
+    @page_count
   end
   def genre=(genre)
    @genre = genre
